3f09b352979c2ac74a159b046e5ae4f5041b0c27
Martino888 <maxmartino33@gmail.com>
2022-02-21 20:55:23 +0100

    biglietto

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-let age1 = prompt("quanti anni hai");
-let km = prompt("quanti km devi percorrere");
+let age1 = prompt("Quanti anni hai");
+let km = prompt("Quanti km devi percorrere");
 const prezzo = 0.21;
-const prezzoMinorenni = 0.168;
-const prezzoOver65 = 0.126;
+const minorenni = 0.21 - 0.042;
+const over65 = 0.21- 0.084;
+
 
 if (age1 < 18) {
-    document.getElementById('my-output').innerHTML = prezzoMinorenni * km;    
+    let prezzoMinorenni = minorenni * km;
+    document.getElementById('under18').innerHTML ="prezzo scontato per under18 &euro;" + prezzoMinorenni.toFixed(2);   
 } else if(age1 > 65){
-    document.getElementById('my-output').innerHTML = prezzoOver65 * km; 
+    let prezzoOver65 = over65 * km;
+    document.getElementById('over65').innerHTML ="Prezzo scontato per  over65 &euro;" + prezzoOver65.toFixed(2); 
+} else{
+    document.getElementById('ticket-price').innerHTML = prezzo * km;
 } 
+
+document.getElementById('ticket-price').innerHTML = `
+<!-- stampa dati utente -->
+<div>
+    <span>Distanza coperta: ${km} Km - Anni: ${age1}</span>
+<hr>
+</div>
+<!-- stampa dettagli prezzo -->
+<div>
+    <p>Sconti: 20% per i minorenni - 40% per gli over 65</p>
+    <span>Prezzo base km : ${prezzo} &euro;</span>
+    <hr>
+</div>
+<div>
+<strong>Prezzo complessivo: ${km * prezzo } &euro;</strong>
+</div>
+`;
